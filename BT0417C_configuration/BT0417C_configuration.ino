e1f6f44d8df277c0782ca1568d1e24c634ad5aaa
Daniel Sikar <dsikar@gmail.com>
2016-01-28 18:33:09 -0200

    Updated .ino and .md

--- BT0417C_configuration/BT0417C_configuration.ino
+++ BT0417C_configuration/BT0417C_configuration.ino
@@ -1,22 +1,3 @@
-/*
-  Software serial multple serial test
- 
- Receives from the hardware serial, sends to software serial.
- Receives from software serial, sends to hardware serial.
- 
- The circuit: 
- * RX is digital pin 2 (connect to TX of other device)
- * TX is digital pin 3 (connect to RX of other device)
- 
- created back in the mists of time
- modified 9 Apr 2012
- by Tom Igoe
- based on Mikal Hart's example
- 
- This example code is in the public domain.
- 
- */
- 
  /*
    Notes on using this code to pair the BT0417C with the Arduino Uno
    
@@ -30,32 +11,14 @@
    5. Response to command AT+NAMEyourname is OKsetname, not OKyourname as per datasheet.
    
  */
+ 
 #include <SoftwareSerial.h>
 
 #define RX   2
 #define TX   3
 
-// SoftwareSerial mySerial(2, 3); // RX, TX
-SoftwareSerial mySerial(RX, TX); // RX, TX
-int led = 7;
-int yellowled = 13;
+SoftwareSerial mySerial(RX, TX);
 String txtMsg = ""; 
-
-void blinkred()
-{
-  digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);               // wait for a second
-  digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000);               // wait for a second
-}
-
-void blinkyellowled()
-{
-  digitalWrite(yellowled, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);               // wait for a second
-  digitalWrite(yellowled, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000);               // wait for a second
-}
 
 void read_serial() {
   // one second delay for module to deal with incoming data
@@ -67,56 +30,100 @@ void read_serial() {
   Serial.write("\n------\n"); 
 }
 
+/*********************************
+  Set up device
+*********************************/
+
+void BT0417C_setup(String strDevName, String strPin, String strBaudRate) {
+
+  Serial.println("Sending command AT...");
+  Serial.print("REPLY: ");
+  mySerial.write("AT");
+  read_serial(); 
+  
+  Serial.println("Sending command AT+NAME...");
+  Serial.print("REPLY: ");  
+  String strParam = "AT+NAME" + strDevName;
+  char* buf = (char*) malloc(sizeof(char)*strParam.length()+1);
+  int iStrLen = strParam.length()+1;
+  mySerial.write(buf, iStrLen);
+  free(buf);
+  
+  read_serial(); 
+  Serial.println("Sending command AT+PIN...");
+  Serial.print("REPLY: ");
+  strParam = "AT+PIN" + strPin;
+  buf = (char*) malloc(sizeof(char)*strParam.length()+1);
+  iStrLen = strParam.length()+1;
+  mySerial.write(buf, iStrLen);
+  read_serial();
+  
+  Serial.println("Sending command AT+BAUD7 (57600)...");
+  Serial.print("REPLY: "); 
+  strParam = "AT+" + strBaudRate;
+  buf = (char*) malloc(sizeof(char)*strParam.length()+1);  
+  iStrLen = strParam.length()+1;
+  mySerial.write(buf, iStrLen);
+  
+  read_serial();  
+  
+}
+
 void setup()  
 {
-  pinMode(led, OUTPUT);
-  pinMode(yellowled, OUTPUT);
- 
   // Open serial communications and wait for port to open:
-  //Serial.begin(57600);
+  // Serial.begin(57600);
   // Default serial baud rate = 9600, optimal Android baud rate (according to Amarino documentation): 57600
   Serial.begin(9600);
   // set the data rate for the SoftwareSerial port
+  // This must match the current baud rate on the device
   mySerial.begin(57600);
   Serial.println("STARTING serial communication with BT0417C device...");
+  BT0417C_setup("med_hum_v0.1", "0123", "BAUD7");
+}
 
-  // if more than one serial communication channel (RX + TX pins) connected, listen explicitly the desired channel
-  //mySerial.listen(); 
+void setup()  
+{
+  // Set hardware serial baud rate
+  Serial.begin(9600);
+  // Set software serial baud rate
+  mySerial.begin(57600);
+  Serial.println("STARTING serial communication with BT0417C device...");
   
   Serial.println("Sending command AT...");
   Serial.print("REPLY: ");
   mySerial.write("AT");
   read_serial(); 
   Serial.println("Sending command AT+NAME...");
-  Serial.print("REPLY: ");  
+  Serial.print("REPLY: "); 
+   
   mySerial.write("AT+NAMEmed_hum_v0.1");
   read_serial(); 
   Serial.println("Sending command AT+PIN...");
   Serial.print("REPLY: ");
   mySerial.write("AT+PIN0123");
   read_serial();   
-  Serial.println("Sending command AT+BAUD7 (57600)...");
+  Serial.println("Sending command AT+BAUD7 (57600)..."); // 57600 Optimal Android Bluetooth baud rate - discuss
   Serial.print("REPLY: ");  
   mySerial.write("AT+BAUD7");
   read_serial();
 }
 
-void loop() // run over and over
+void loop()
 {
-
-  //while (Serial.available() > 0) {
-    while (mySerial.available() > 0) {
+  // Connectivity tests
+  
+  // TODO Send messages coming in via Serial Monitor to Bluetooth module and beyond, which should happened if it is paired and connected.
+ 
+ 
+  // Send message coming in via Bluetooth monitor to Serial Monitor
+   while (mySerial.available() > 0) {
     //char inChar = Serial.read();
     char inChar = mySerial.read();
     txtMsg += inChar;
   }
   if (txtMsg.length() > 0) {
-    if(txtMsg.substring(1,3) == "on") {
-      digitalWrite(led, HIGH);
-    }
-    if(txtMsg.substring(1,4) == "off") {
-      digitalWrite(led, LOW);
-   }      
+    
     Serial.println(txtMsg);
     mySerial.write("Message received ok");
     txtMsg = "";
--- README.md
+++ README.md
@@ -8,17 +8,24 @@ At the heart of this breakout board, manufactured by mdfly.com is the CSR BC417 
 
 # Configuration
 
-To configure the module, wire the breakout board to an Anduino Uno, load the sketch and change the parameters to suit your application.
+To configure the module, first wire the breakout board to an Anduino Uno, load the sketch and change the parameters to suit your application.
 
 ## Wiring
 
 ![BT0417C Wiring](images/BT0417C-wiring.png)
 
-Wire ground (green) and +5V (red) from your breadboard to the UNO (the actual labels will be written at the back of the BT0417C). Once the module is powered up, a blinking LED indicates that Bluetooth radio is functional but device is not paired. Once it is paired, the LED will stay on and stop blinking.  
+Wire ground (green) and +5V (red) from your breadboard to the UNO (the actual labels are written at the back of the BT0417C), then TX to 2 and RX to 3. Once the module is powered up, a blinking LED indicates that Bluetooth radio is functional but device is not paired. Once it is paired, the LED will stay on and stop blinking. Note, the UNO must also be connected. 
 
 ## Configuring
 
-
+Load the sketch into your IDE. In setup(), edit four-digit PIN and name (57600 baud rate works ok with Android) then compile and upload.
 
 ## Testing
 
+Once paired and connected, messages sent from Android (Bluetooth monitoring app of your choice required) should turn up at Serial Monitor and vice-versa.
+
+## Datasheets
+
+TODO  
+CSR  
+MDFLY x2
