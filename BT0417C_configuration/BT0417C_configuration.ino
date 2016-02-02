5449d43c23f13193852f2cb87deb258de9b1bfd8
Daniel Sikar <dsikar@gmail.com>
2016-02-02 15:39:22 -0200

    Added connectivity tests.

--- BT0417C_configuration/BT0417C_configuration.ino
+++ BT0417C_configuration/BT0417C_configuration.ino
@@ -6,7 +6,7 @@
    1. Pins 0 and 1 did not work as RX, TX.
    2. Pins 2 and 3 worked as RX, TX. Not only did they work, uploading sketches also worked, as opposed to pins 0 and 1, where the wires had to be
    disconnected on every compile and upload. IMPORTANT: Connect Arduino RX to Bluetooth adapter TX and Arduino TX to Bluetooth adapter RX.
-   3. A delay was required after every serial write to the BT0417C,
+   3. A delay was required to assemble messages typed into Serial Monitor without introducting line breaks.
    4. Default baud rate for the BT0417C is 9600.
    5. Response to command AT+NAMEyourname is OKsetname, not OKyourname as per datasheet.
    
@@ -36,6 +36,7 @@ void setup()
   Serial.begin(9600);
   // Set software serial baud rate
   mySerial.begin(57600);
+  
   Serial.println("STARTING serial communication with BT0417C device...");
   
   Serial.println("Sending command AT...");
@@ -55,27 +56,46 @@ void setup()
   Serial.print("REPLY: ");  
   mySerial.write("AT+BAUD7");
   read_serial();
+  
 }
 
 void loop()
 {
-  // Connectivity tests
+  // Connectivity tests.
+  // Tested with Amarino and Bluetooth Viewer (LITE).
+  // Note, the BT0417C must NOT be *paired* while code is uploading.
   
-  // TODO Send messages coming in via Serial Monitor to Bluetooth module and beyond, which should happened if it is paired and connected.
- 
- 
-  // Send message coming in via Bluetooth monitor to Serial Monitor
-   while (mySerial.available() > 0) {
+  // 1. SEND - Assemble message sent via Serial Monitor
+  while (Serial.available() > 0) {
+    char inChar = Serial.read();
+    txtMsg += inChar;
+  }
+  // Delay used for human readability so we don't split messages
+  // typed in Serial Monitor. 
+  // If application at the other end can identify beginning and end
+  // of message such as delimiters, delay is not required.
+  delay(200);
+  if (txtMsg.length() > 0) {
+    // Echo message to Serial Monitor. 
+    Serial.print("BT0417C sending message: ");
+    Serial.println(txtMsg);
+    // Send to BT0417C
+    mySerial.print("BT0417C sending message: ");
+    mySerial.println(txtMsg);
+    txtMsg = "";
+  }
+  // 2. RECEIVE - Assemble message received by BT0417C
+  while (mySerial.available() > 0) {
     //char inChar = Serial.read();
     char inChar = mySerial.read();
     txtMsg += inChar;
   }
   if (txtMsg.length() > 0) {
-    
-    Serial.println(txtMsg);
-    mySerial.write("Message received ok");
+    // Print incoming message received by BT0417C to Serial Monitor then echo back to sender.
+    Serial.print("BT0417C received message: ");
+    Serial.println(txtMsg);  
+    mySerial.print("BT0417C received message: ");
+    mySerial.println(txtMsg);
     txtMsg = "";
-    
-    delay(2000);
   }
 }
